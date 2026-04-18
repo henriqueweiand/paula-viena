83db89044bf787738468cdd0bc30d830e87ae177
henrique weiand <henriqueweiand@gmail.com>
2026-04-18 10:21:19 -0300

    base project

--- app/layout.tsx
+++ app/layout.tsx
@@ -112,7 +112,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} suppressHydrationWarning>{children}</body>
     </html>
   )
 }
